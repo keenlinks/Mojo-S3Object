8b2124818a411cc58663f574efa082796bb696cd
Scott Kiehn <keenlinks@gmail.com>
2017-02-28 19:49:01 -0600

    add some non-blocking code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mojolicious::Plugin::S3Request
+
+Very early development code (although I use currently for my projects). A lot to be done (DRY, abstraction, tests). I wanted a basic S3 helper for objects, especially a policy producer for browser-based uploads.
--- lib/Mojolicious/Plugin/S3Request.pm
+++ lib/Mojolicious/Plugin/S3Request.pm
@@ -3,8 +3,8 @@ package Mojolicious::Plugin::S3Request;
 use Mojo::Base 'Mojolicious::Plugin';
 use Digest::SHA qw(hmac_sha256 hmac_sha256_hex sha256_hex);
 use Mojo::Date;
+use Mojo::IOLoop;
 use Mojo::JSON qw(encode_json);
-use Mojo::UserAgent;
 use Mojo::Util qw(monkey_patch trim);
 
 monkey_patch 'Mojo::Date', to_ymd => sub {
@@ -17,12 +17,13 @@ monkey_patch 'Mojo::Date', to_ymdhms => sub {
 	sprintf '%04d%02d%02dT%02d%02d%02dZ', $gmtime[5] + 1900, $gmtime[4] + 1, $gmtime[3], $gmtime[2], $gmtime[1], $gmtime[0];
 };
 
-our $VERSION = '0.001';
+our $VERSION = '0.002';
 
-my $AWS_AUTH_ALG = 'AWS4-HMAC-SHA256';
-my $AWS_HOST = 's3.amazonaws.com';
-my $AWS_REQUEST = 'aws4_request';
-my $AWS_SERVICE = 's3';
+has ioloop => sub { Mojo::IOLoop->new };
+has aws_auth_alg => 'AWS4-HMAC-SHA256';
+has aws_host => 's3.amazonaws.com';
+has aws_request => 'aws4_request';
+has aws_service => 's3';
 
 sub register {
 	my ( $self, $app, $conf ) = @_;
@@ -43,20 +44,31 @@ sub register {
 	});
 
 	$app->helper( 's3.get_file' => sub {
+		my $cb = ref $_[-1] eq 'CODE' ? pop : undef;
 		my ( $c, $filename ) = @_;
 		my $date = Mojo::Date->new;
 
 		my $headers = $self->_headers( $date );
 		$headers->{authorization} = $self->_auth_header( 'GET', $date, $filename, $headers );
 
-		my $tx = $c->ua->get( $self->{conf}->{protocol} . $self->_bucket_path . '/' . $filename => $headers );
-		#say 'GET: ' . $tx->res->code;
-
-		return $tx->res if $tx->res->code == 200;
-		return;
+		if ( $cb ) {
+			$self->_loop->delay(
+				sub {
+					$c->ua->get( $self->{conf}->{protocol} . $self->_bucket_path . '/' . $filename => $headers, shift->begin );
+				},
+				sub {
+					my $tx = pop;
+					return $self->$cb( $tx->res->code == 200 ? $tx->res : undef );
+				}
+			)->wait;
+		} else {
+			my $tx = $c->ua->get( $self->{conf}->{protocol} . $self->_bucket_path . '/' . $filename => $headers );
+			return $tx->res->code == 200 ? $tx->res : undef;
+		}
 	});
 
 	$app->helper( 's3.put_file' => sub {
+		my $cb = ref $_[-1] eq 'CODE' ? pop : undef;
 		my ( $c, $filename, $size, $type, $content ) = @_;
 		my $date = Mojo::Date->new;
 
@@ -66,25 +78,44 @@ sub register {
 		$headers->{'x-amz-content-sha256'} = sha256_hex( $content );
 		$headers->{authorization} = $self->_auth_header( 'PUT', $date, $filename, $headers );
 
-		my $tx = $c->ua->put( $self->{conf}->{protocol} . $self->_bucket_path . '/' . $filename => $headers => $content );
-		#say 'PUT: ' . $tx->res->code;
-		#say $tx->res->body;
-		return 1 if $tx->res->code == 200;
-		return;
+		if ( $cb ) {
+			$self->_loop->delay(
+				sub {
+					$c->ua->put( $self->{conf}->{protocol} . $self->_bucket_path . '/' . $filename => $headers => $content, shift->begin );
+				},
+				sub {
+					my $tx = pop;
+					return $self->$cb( $tx->res->code == 200 ? $tx->res : undef );
+				}
+			)->wait;
+		} else {
+			my $tx = $c->ua->put( $self->{conf}->{protocol} . $self->_bucket_path . '/' . $filename => $headers => $content );
+			return $tx->res->code == 200 ? $tx->res : undef;
+		}
 	});
 
 	$app->helper( 's3.delete_file' => sub {
+		my $cb = ref $_[-1] eq 'CODE' ? pop : undef;
 		my ( $c, $filename ) = @_;
 		my $date = Mojo::Date->new;
 
 		my $headers = $self->_headers( $date );
 		$headers->{authorization} = $self->_auth_header( 'DELETE', $date, $filename, $headers );
 
-		my $tx = $c->ua->delete( $self->{conf}->{protocol} . $self->_bucket_path . '/' . $filename => $headers );
-		#say 'DELETE: ' . $tx->res->code;
-		#say $tx->res->body;
-		return 1 if $tx->res->code == 204;
-		return;
+		if ( $cb ) {
+			$self->_loop->delay(
+				sub {
+					$c->ua->delete( $self->{conf}->{protocol} . $self->_bucket_path . '/' . $filename => $headers, shift->begin );
+				},
+				sub {
+					my $tx = pop;
+					return $self->$cb( $tx->res->code == 200 ? $tx->res : undef );
+				}
+			)->wait;
+		} else {
+			my $tx = $c->ua->delete( $self->{conf}->{protocol} . $self->_bucket_path . '/' . $filename => $headers );
+			return $tx->res->code == 204 ? $tx->res : undef;
+		}
 	});
 
 	$app->helper( 's3.move_file' => sub {
@@ -113,7 +144,7 @@ sub register {
 			expiration => Mojo::Date->new( time + 900 )->to_datetime,
 			conditions => [
 				{bucket => $self->{conf}->{bucket}},
-				{'x-amz-algorithm' => $AWS_AUTH_ALG},
+				{'x-amz-algorithm' => $self->aws_auth_alg},
 				{'x-amz-credential' => $credential},
 				{'x-amz-date' => $date->to_ymdhms},
 				{acl => 'public-read'},
@@ -127,7 +158,7 @@ sub register {
 		my $base64_policy = $c->b( $policy )->b64_encode('');
 
 		return {
-			'x-amz-algorithm' => $AWS_AUTH_ALG,
+			'x-amz-algorithm' => $self->aws_auth_alg,
 			'x-amz-credential' => $credential,
 			'x-amz-date' => $date->to_ymdhms,
 			acl => 'public-read',
@@ -152,14 +183,14 @@ sub _auth_header {
 
 	my $signature = $self->_sign_the_string( $string_to_sign, $self->{conf}->{access_key}, $date->to_ymd );
 
-	my $auth = $AWS_AUTH_ALG;
+	my $auth = $self->aws_auth_alg;
 	$auth .= ' Credential=' . $self->_credential( $date ) . ',';
 	$auth .= 'SignedHeaders=' . ( join ';', sort keys %$headers ) . ',';
 	$auth .= 'Signature=' . $signature;
 }
 
 sub _bucket_path {
-	$_[0]->{conf}->{bucket} . '.' . $AWS_HOST;
+	$_[0]->{conf}->{bucket} . '.' . $_[0]->aws_host;
 }
 
 sub _canonical_request {
@@ -173,7 +204,7 @@ sub _canonical_request {
 }
 
 sub _credential {
-	$_[0]->{conf}->{access_key_id} . '/' . $_[1]->to_ymd . '/' . $_[0]->{conf}->{region} . "/$AWS_SERVICE/$AWS_REQUEST";
+	$_[0]->{conf}->{access_key_id} . '/' . $_[1]->to_ymd . '/' . $_[0]->{conf}->{region} . '/' . $_[0]->aws_service . '/' . $_[0]->aws_request;
 }
 
 sub _headers {
@@ -184,18 +215,12 @@ sub _headers {
 	};
 }
 
-sub _string_to_sign {
-	my ( $self, $canonical_request, $date ) = @_;
-	my $string_to_sign = "$AWS_AUTH_ALG\n";
-	$string_to_sign .= $date->to_string . "\n";
-	$string_to_sign .= $date->to_ymd . '/' . $self->{conf}->{region} . "/$AWS_SERVICE/$AWS_REQUEST\n";
-	$string_to_sign .= sha256_hex( $canonical_request );
-}
+sub _loop { $_[1] ? Mojo::IOLoop->singleton : $_[0]->ioloop }
 
 sub _signing_key {
 	my $self = shift;
-	hmac_sha256( $AWS_REQUEST,
-		hmac_sha256( $AWS_SERVICE,
+	hmac_sha256( $self->aws_request,
+		hmac_sha256( $self->aws_service,
 			hmac_sha256( $self->{conf}->{region},
 				hmac_sha256( Mojo::Date->new->to_ymd, 'AWS4' . $self->{conf}->{access_key} )
 			)
@@ -207,8 +232,15 @@ sub _sign_the_string {
 	hmac_sha256_hex( $_[1], $_[0]->_signing_key );
 }
 
-1;
+sub _string_to_sign {
+	my ( $self, $canonical_request, $date ) = @_;
+	my $string_to_sign = $self->aws_auth_alg . "\n";
+	$string_to_sign .= $date->to_string . "\n";
+	$string_to_sign .= $date->to_ymd . '/' . $self->{conf}->{region} . '/' . $self->aws_service . '/' . $self->aws_request . "\n";
+	$string_to_sign .= sha256_hex( $canonical_request );
+}
 
+1;
 __END__
 
 =encoding utf-8
@@ -241,4 +273,3 @@ it under the same terms as Perl itself.
 =head1 SEE ALSO
 
 =cut
-
